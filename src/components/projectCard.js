92867a38e34088b5245f188d8c63a07370a0df32
Andrew Sadikov <andrewsadikov@Andrews-MBP.verizon.net>
2022-05-15 19:23:25 -0400

    Finished Projects section

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,14 +1,39 @@
 const ProjectCard = (props) => {
   return (
-    <div className="card">
-      <p>{props.name}</p>
-      <p>{props.about}</p>
-      <a href={props.live} target="_blank" rel="noreferrer">
-        Live App
-      </a>
-      <a href={props.code} target="_blank" rel="noreferrer">
-        Code
-      </a>
+    <div className="proj-card">
+      <img
+        className="proj-image"
+        alt=""
+        src={require(`../images/${props.image}`)}
+      />
+      <div className="proj-info">
+        <h1 className="proj-title">{props.name}</h1>
+        <p
+          className={props.darkMode ? "exp-card-about" : "exp-card-about-dark"}
+        >
+          {props.about}
+        </p>
+        <div className="proj-button-section">
+          <a href={props.live} target="_blank" rel="noreferrer">
+            <button
+              className={
+                props.darkMode ? "proj-button-live" : "proj-button-live-dark"
+              }
+            >
+              Live App
+            </button>
+          </a>
+          <a href={props.code} target="_blank" rel="noreferrer">
+            <button
+              className={
+                props.darkMode ? "proj-button-code" : "proj-button-code-dark"
+              }
+            >
+              Code
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,15 +1,27 @@
 const projectData = [
   {
-    name: "First React App",
-    about: "First app I ever made using react",
-    live: "https://www.google.com",
-    code: "https://www.google.com",
+    name: "Personal Portfolio App",
+    about:
+      "My first webapp using react. Dynamically maps components and passes data.",
+    live: "https://andrewsadikov.com",
+    code: "https://github.com/AndrewSadikov/ReactPortfolio-WIP",
+    image: "portfolio.png",
   },
   {
-    name: "John Smith",
-    about: "I am john smith",
-    live: "https://www.google.com",
-    code: "https://www.google.com",
+    name: "Tours App",
+    about:
+      "Tours app. Used to dynamically load content from asynchronous API calls using react hooks.",
+    live: "https://tours.andrewsadikov.com",
+    code: "https://github.com/AndrewSadikov/ReactToursApp",
+    image: "tours.png",
+  },
+  {
+    name: "First React App",
+    about:
+      "My first webapp using react. Dynamically maps components and passes data.",
+    live: "https://first.andrewsadikov.com",
+    code: "https://github.com/AndrewSadikov/ReactFirstApp",
+    image: "first.png",
   },
 ];
 
